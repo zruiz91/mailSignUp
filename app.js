349f50a1d2f243fd0328aafb9d2c3da9ae7351a3
Zach Ruiz <powersbooth10@gmail.com>
2023-05-08 12:29:58 -0500

    configuring post request for sunscribing users to mailing list.

--- app.js
+++ app.js
@@ -23,11 +23,46 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
 
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let email = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.,ailchimp.com/3.0/lists/bf207e9252";
+
+    const options = {
+        method: "POST",
+        auth: "zach1:4b8d678e897e0eba2b4ad7b29c896963-us21"
+    }
+
+    https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    })
 
     console.log(firstName);
     console.log(lastName);
     console.log(email);
 });
+
+
+// Api Key
+// 4b8d678e897e0eba2b4ad7b29c896963-us21
+
+// audience id
+// bf207e9252
